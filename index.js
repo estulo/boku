dda75919a3fe8b48ea0ddc099a83a61223ef9d60
estulo <estulo.v0@gmail.com>
2023-08-01 17:22:31 -0500

    added main menu animations

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-function btnAnim() {
-    console.log(document.getElementsByClassName('back-panel'))
+var last_op = 1;
+
+function btnAnim(menu) {
+    let [back, front, text] = menu.children;
+    
+    front.classList.toggle('front-panel-click');
+    text.classList.toggle('move-txt-click');
+    back.classList.toggle('back-panel-click');
+}
+
+
+function btnHover(menu) {
+    let [back, front, text] = menu.children;
+    front.classList.toggle('front-panel');
+    front.classList.toggle('front-panel-hover');
+
+    back.classList.toggle('back-panel');
+    back.classList.toggle('back-panel-hover');
+
+    text.classList.toggle('move-txt');
+    text.classList.toggle('move-txt-hover');
+
+
 }
